e98b6b039e2990f15e0a6b64ebfdd3928ba51fbf
chirix33 <chirixteam@gmail.com>
2024-11-26 13:52:44 -0600

    Enhance scraper.py with HTML content extraction and cleaning functions

--- scraper.py
+++ scraper.py
@@ -1,15 +1,55 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 
 def scrape_link(link):
     chrome_driver_path = "./chromedriver.exe"
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+    html = ""
 
     try:
         driver.get(link)
-        return driver.page_source
+        html = driver.page_source
     finally:
         driver.quit()
 
-result = scrape_link("https://www.amazon.com")
+    return html
+
+def get_body_content(html):
+    soup = BeautifulSoup(html, 'lxml')
+    body = soup.body
+    if body:
+        return str(body)
+    else:
+        return ""
+    
+
+def clean_html(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    for script in soup(["script", "style"]):
+        script.extract()
+    
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+    return cleaned_content
+
+
+def split_content(content, max_length=6000):
+    return [
+        content[i: i + max_length] for i in range(0, len(content), max_length)
+    ]
+
+
+DIR = "temp_files/source_code.txt"
+print("Scraping...")
+result = scrape_link("https://greatwaysinc.com/")
+content = get_body_content(result)
+cleaned_content = clean_html(content)
+
+with open(DIR, "w", encoding="utf-8") as file:
+    file.write(cleaned_content)
+    file.write("\n\n")
+
+print("Scraping done!")
